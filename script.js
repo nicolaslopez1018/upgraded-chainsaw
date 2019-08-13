1576e2481abf97f3beb33e0f3d4a6f1b28943c5d
velamds <velamds@gmail.com>
2019-08-13 12:22:22 -0500

    validacion de campo para que no sea vacio

--- script.js
+++ script.js
@@ -3,7 +3,14 @@ function htmlEncode (value){
 }
 
 $(function() {
+  $("#Mensaje").hide();
   $("#generate").click(function() {
+    $("#Mensaje").hide();
+    valorQR = $("#content").val() == "" ? false : true;
+    if(!valorQR){
+      $("#Mensaje").fadeIn();
+      return false;
+    }
     $(".qr-code").attr("src", "https://chart.googleapis.com/chart?cht=qr&chl=" + htmlEncode($("#content").val()) + "&chs=160x160&chld=L|0");
   });
 });
